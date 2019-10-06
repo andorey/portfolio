9d95373d4fd94c4ae52d83923704d817b70a8332
andoreyrich <andoreyrich@gmail.com>
2019-10-06 13:17:46 +0200

    test

---
+++ src/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Main.module.css'
+
+function Main() {
+  return (
+    <div className={styles.header}>
+
+    </div>
+  );
+}
+
+export default Main;
