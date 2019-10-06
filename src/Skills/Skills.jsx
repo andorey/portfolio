e97ea3a88fa2b347fd3c1168da6aa37fe26404ee
andoreyrich <andoreyrich@gmail.com>
2019-10-06 16:27:57 +0200

    second block

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import styles from './Skills.module.css'
 
 function Skills() {
-  return (
-    <div className={styles.header}>
-
-    </div>
-  );
+	return (
+		<div className={styles.header}>
+			<div className={styles.block}>
+				<div className={styles.up}>
+					My Skills
+				</div>
+				<div className={styles.third}>
+					<div> </div>
+					<div> </div>
+					<div> </div>
+				</div>
+            </div>
+		</div>
+	);
 }
 
 export default Skills;
