f3ce2c607fd7b3321fed87142d99110530ea130d
andoreyrich <andoreyrich@gmail.com>
2019-10-06 17:57:11 +0200

    Updates

---
+++ precache-manifest.fa3596a2e3913815693d2bd802af850e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2366a0cd2a43889f92442861063cf5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8c31d4e70f371eab1879",
+    "url": "/portfolio/static/css/main.5be49455.chunk.css"
+  },
+  {
+    "revision": "219093345655105503b8",
+    "url": "/portfolio/static/js/2.0c507a4b.chunk.js"
+  },
+  {
+    "revision": "8c31d4e70f371eab1879",
+    "url": "/portfolio/static/js/main.e7f15c41.chunk.js"
+  },
+  {
+    "revision": "e64e0e6288a84de2bcd0",
+    "url": "/portfolio/static/js/runtime-main.b15c24f7.js"
+  }
+]);
