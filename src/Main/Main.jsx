6481f05ace100ae44b27a31675c51b8720a9f3da
andoreyrich <andoreyrich@gmail.com>
2019-10-06 15:34:32 +0200

    first block

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import styles from './Main.module.css'
 
 function Main() {
-  return (
-    <div className={styles.header}>
-
-    </div>
-  );
+	return (
+		<div className={styles.header}>
+			<div className={styles.block}>
+				<div className={styles.greeting}>
+					<span>Hello!</span>
+					<span>My name's Andorey Rich</span>
+					<span>I'm front-end Developer</span>
+				</div>
+				<div className={styles.photo}> </div>
+			</div>
+		</div>
+	);
 }
 
 export default Main;
