31fceba42c32a20e273486cf51b773dd336d7a41
andoreyrich <andoreyrich@gmail.com>
2019-10-06 22:55:32 +0200

    Updates

---
+++ precache-manifest.9a5c1ce5fe081230f7c34b361da9231f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d41c750b7d523e200af51ed5ee82480",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "60b8c66417a810afc000",
+    "url": "/portfolio/static/css/main.0d5b80a9.chunk.css"
+  },
+  {
+    "revision": "ed499d715926270f35e2",
+    "url": "/portfolio/static/js/2.1463566c.chunk.js"
+  },
+  {
+    "revision": "60b8c66417a810afc000",
+    "url": "/portfolio/static/js/main.8a50e80c.chunk.js"
+  },
+  {
+    "revision": "e64e0e6288a84de2bcd0",
+    "url": "/portfolio/static/js/runtime-main.b15c24f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.20b1f71b82adcf571bcdee81dde51a05.js"
+  "/portfolio/precache-manifest.9a5c1ce5fe081230f7c34b361da9231f.js"
 );
 
 self.addEventListener('message', (event) => {
