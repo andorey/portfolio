4af0daab06eb2d05f472955a6625b76fb92561f5
andoreyrich <andoreyrich@gmail.com>
2019-10-12 22:12:12 +0200

    trying repair footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,16 +3,16 @@ import styles from './Footer.module.css'
 
 function Footer() {
 	return (
-		<div className={styles.header}>
+		<div className={styles.footer}>
 			<div className={styles.block}>
 				<div>
 					Andorey Rich
 				</div>
 				<div className={styles.contacts}>
-					<div> </div>
-					<div> </div>
-					<div> </div>
-					<div> </div>
+					<nav> </nav>
+					<nav> </nav>
+					<nav> </nav>
+					<nav> </nav>
 				</div>
 				<div>
 					© 2019 All rights reserved.
