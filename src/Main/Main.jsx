916543b2e4e93c770cfb386b8d6a5f366eca726e
andoreyrich <andoreyrich@gmail.com>
2019-10-28 00:32:35 +0100

    do not add header of site, try #2

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -7,8 +7,8 @@ function Main() {
 			<div className={styles.block}>
 				<div className={styles.greeting}>
 					<span>Hello!</span>
-					<span>My name's Andorey Rich</span>
-					<span>I'm front-end Developer</span>
+					<span className={styles.name}>My name's Andorey Rich</span>
+					<h1>I'm front-end Developer</h1>
 				</div>
 				<div className={styles.photo}> </div>
 			</div>
